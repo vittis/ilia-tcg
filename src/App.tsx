309dc53756991b118fe05720db22684df22de5b6
vittis <vitbolb1@gmail.com>
2024-07-08 17:39:46 -0300

    feat: add shadcn initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { api } from "./services/http/axios";
+import { Button } from "@/components/ui/button";
+import { api } from "@/services/http/axios";
+import {
+	DialogHeader,
+	DialogFooter,
+	Dialog,
+	DialogTrigger,
+	DialogContent,
+	DialogTitle,
+	DialogDescription,
+} from "@/components/ui/dialog";
 
 const fetchCards = async () => {
 	return await api.get("/cards");
@@ -8,12 +18,28 @@ const fetchCards = async () => {
 function App() {
 	const { data } = useQuery({ queryKey: ["cards"], queryFn: fetchCards });
 
-	console.log(data);
-
 	return (
-		<>
+		<div>
 			<div className="text-3xl text-blue-500">Tailwind test</div>
-		</>
+			<Button variant="default">Test</Button>
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button variant="outline">Edit Profile</Button>
+				</DialogTrigger>
+				<DialogContent className="sm:max-w-[425px]">
+					<DialogHeader>
+						<DialogTitle>Edit profile</DialogTitle>
+						<DialogDescription>
+							Make changes to your profile here. Click save when you're done.
+						</DialogDescription>
+					</DialogHeader>
+					<div className="grid gap-4 py-4">oi</div>
+					<DialogFooter>
+						<Button type="submit">Save changes</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
+		</div>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./styles/main.scss";
+import "react-toastify/dist/ReactToastify.min.css";
+import "./styles/main.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./services/http/queryClient.tsx";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
